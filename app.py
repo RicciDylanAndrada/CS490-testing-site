2e42c6b9fef093ffa4d4cfc34957efe7ffae59bd
Ricci <riccidandrada@gmail.com>
2022-02-24 11:59:56 -0500

    flask/django

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from venv import create
-from flask import Flask
+from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token,get_jwt,get_jwt_identity, \
+                               unset_jwt_cookies, jwt_required, JWTManager
 db = SQLAlchemy()
 migrate = Migrate()
 ma = Marshmallow()
 cors = CORS()
+
 def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
@@ -31,6 +34,13 @@ def index():
     return {
         "testing":"Hello from flask backend"
     }
+@app.route('/profile')
+def my_profile():
+    response_body = {
+        "name": "Ricci",
+        "about" :"Testing for this link"
+    }
 
+    return response_body
 if __name__=="__main__":
     app.run(debug=True)
